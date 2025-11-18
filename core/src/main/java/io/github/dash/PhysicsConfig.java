33c9cbc8856dc11d8ece924b4b5f8745c26c4071
mel <mel.barruel-brussin@etu.unice.fr>
2025-11-18 16:23:29 +0100

    phisic test

---
+++ core/src/main/java/io/github/dash/PhysicsConfig.java
@@ -0,0 +1,23 @@
+package io.github.dash;
+
+public class PhysicsConfig {
+
+    // vitesse horizontale du cube
+    public static final float FORWARD_SPEED = 300f;
+
+    // gravité
+    public static final float GRAVITY = -2000f;
+
+    // vitesse verticale initiale quand on saute
+    public static final float JUMP_SPEED = 900f;
+
+    // position du sol
+    public static final float GROUND_Y = 100f;
+
+    // vitesse de chute max
+    public static final float MAX_FALL_SPEED = -2500f;
+
+    private PhysicsConfig() {
+        // on ne veut pas instancier cette classe
+    }
+}
