1d6773167859fdd411b3d6c7af74fafb6ce7a962
Gertis <royvassili@gmail.com>
2025-12-13 17:27:37 +0100

    Delete assets/geometry_tiles.tsx

--- assets/geometry_tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="geometry_tiles" tilewidth="32" tileheight="32" tilecount="1536" columns="48">
- <image source="../../../Downloads/ChatGPT Image 13 déc. 2025, 16_00_34.png" width="1536" height="1024"/>
-</tileset>
